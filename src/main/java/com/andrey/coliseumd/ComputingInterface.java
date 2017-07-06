47d7afa231dc9fc0892eca4abfcb31f5bd0d339f
COLISEUMc <andreycoliseum@gmail.com>
2017-07-06 18:07:30 +0300

    Delete ComputingInterface.java

--- src/main/java/com/andrey/coliseumd/ComputingInterface.java
+++
@@ -1,9 +0,0 @@
-package com.andrey.coliseumd;
-
-public interface ComputingInterface {
-
-	public void changeRamMemory(int newValue);
-	public void changeCPUmemory(int time);
-	public void changeVideoMemory(int speed);
-}
-
